56ada9d06397698f43755b5c321f67f26b5d6479
Gautham Krishna <90638995+gauthking@users.noreply.github.com>
2022-09-26 20:19:45 +0530

    Update hardhat.config.js

--- src/contract/hardhat.config.js
+++ src/contract/hardhat.config.js
@@ -12,11 +12,11 @@ module.exports = {
     },
     ropsten: {
       url: "https://ropsten.infura.io/v3/18357c43a5ec4ef8884cb156adcea32b",
-      accounts: ["0x91066c6f168f87e0384ba93c5ae8995f887fa0527534d11f9c005d360d571f10"]
+      accounts: [""]
     },
     ftmtest: {
       url: "https://rpc.testnet.fantom.network",
-      accounts: ["0x91066c6f168f87e0384ba93c5ae8995f887fa0527534d11f9c005d360d571f10"],
+      accounts: [""],
       chainId: 4002
     }
   },
