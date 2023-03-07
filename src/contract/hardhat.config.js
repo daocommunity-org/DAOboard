849f9f6d1fa2e1e1dcc769e4d1b872b82c2379e7
Gautham Krishna <90638995+gauthking@users.noreply.github.com>
2023-03-08 00:34:38 +0530

    Update hardhat.config.js

--- src/contract/hardhat.config.js
+++ src/contract/hardhat.config.js
@@ -20,7 +20,7 @@ module.exports = {
     },
     mumbai: {
       url: "https://rpc-mumbai.maticvigil.com",
-      accounts: ["91066c6f168f87e0384ba93c5ae8995f887fa0527534d11f9c005d360d571f10"],
+      accounts: [""],
       blockConfirmations: 6,
     },
   },
