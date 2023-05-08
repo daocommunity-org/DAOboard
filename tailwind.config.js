167c01f4847b94889d3b466653339cd3cd175897
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2023-05-08 22:28:58 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'spin-slow': 'spin 5s linear infinite',
+      }
+    },
   },
   plugins: [
 
